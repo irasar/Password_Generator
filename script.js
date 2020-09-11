bbda86a7e9833656ae21c998344465327a3f26f1
Sarah Iraurgui <iraurguisj@gmail.com>
2020-09-11 17:05:42 -0400

    pushes added

--- script.js
+++ script.js
@@ -2,26 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
-
-
-
-
 //array if user wants special characters
 
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 
 
 //array if user wants capital letters
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //array if user wants lower case letters
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 //array of numbers user can use if wanting numbers
-var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 
@@ -37,7 +32,8 @@ var ifUpperCase;
 var ifLowerCase;
 var ifSpecial;
 var ifNumber;
-
+//??
+var userPassword;
 
 function generatePassword() {
   //asking how long does the user wants pass to be
@@ -59,78 +55,79 @@ function generatePassword() {
     var ifUpperCase = confirm("Click OK if you would like to include uppercase characters?");
     //if user clicks OK
     if (ifUpperCase === true) {
-      emptyArray.push(upperCase)
-
-
-      {
-
-
-        for (var i = 0; i < upperCase.length; i++) {
-          // putting all the stuff from specialChar array into emptyArray
-          emptyArray.push(upperCase[i]);
-        }
-
+      for (var i = 0; i < upperCase.length; i++) {
+      emptyArray.push(upperCase[i]);
       }
     }
+
+
+//above one correct b4 i chage all others?
+
     //confirm if user wants lowercase
     var ifLowerCase = confirm("Click OK if you would like to include lowercase characters?");
     //if user clicks ok
     if (ifLowerCase === true) {
       emptyArray.push(lowerCase)
-      {
 
 
-        for (var i = 0; i < lowerCase.length; i++) {
-          // putting all the stuff from specialChar array into emptyArray
-          emptyArray.push(lowerCase[i]);
-        }
 
-
+      for (var i = 0; i < lowerCase.length; i++) {
+        // putting all the stuff from lowerCase array into emptyArray
+        emptyArray.push(lowerCase[i]);
       }
+
+
     }
+
 
     //confirm if user wants special characters
     var ifSpecial = confirm("Click OK if you would like to include special characters?");
     //if user clicks ok
     if (ifSpecial === true) {
       emptyArray.push(ifSpecial)
-      {
 
 
 
-        for (var i = 0; i < specialChar.length; i++) {
-          // putting all the stuff from specialChar array into emptyArray
-          emptyArray.push(specialChar[i]);
-        }
 
-
+      for (var i = 0; i < specialChar.length; i++) {
+        // putting all the stuff from specialChar array into emptyArray
+        emptyArray.push(specialChar[i]);
       }
 
 
     }
+
+
+
 
     //confirm if user wants numbers
     var ifNumber = confirm("Click OK if you would like to include numerical characters?");
     //if user clicks ok
     if (ifNumber === true) {
       emptyArray.push(ifNumber)
-      {
-
-
-        for (var i = 0; i < numerical.length; i++) {
-          // putting all the stuff from specialChar array into emptyArray
-          emptyArray.push(numerical[i]);
 
 
 
+      for (var i = 0; i < numerical.length; i++) {
+        // putting all the stuff from numerical array into emptyArray
+        emptyArray.push(numerical[i]);
 
 
-        }
+
+
 
       }
-
     }
 
+  }
+}
+
+
+
+// looping to build the array with the password
+for (var i = 0; i < userChara; i++) {
+  //choosing random index from empty array
+  passwordArray.push(emptyArray[(Math.floor(Math.random() * emptyArray.length))]);
 
 
 
@@ -139,44 +136,16 @@ function generatePassword() {
 
 
 
+}
+// Write password to the #password input
+function writePassword() {
+  var passwordArray = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+ //? var output = "";
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // looping to build the array with the password
-    for (var i = 0; i < userChara; i++) {
-      //choosing random index from empty array
-      passwordArray.push(emptyArray[(Math.floor(Math.random() * emptyArray.length))]);
-    }
-
-
-
-
-
-    // Write password to the #password input
-    function writePassword() {
-      var passwordArray = generatePassword();
-      var passwordText = document.querySelector("#password");
-
-      passwordText.value = password;
-
-    }
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
