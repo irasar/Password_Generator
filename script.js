443289045ba945d20ccfc5fd4b0653968915380a
Sarah Iraurgui <iraurguisj@gmail.com>
2020-09-11 15:11:13 -0400

    confirms added

--- script.js
+++ script.js
@@ -22,87 +22,60 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 
 
 //array of numbers user can use if wanting numbers
-var numerical = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-
-// combining all arrays
-var allArray = [lowerCase, upperCase, numerical, specialChar];
 
 
 //empty array
-var emptyArray =[];
+var emptyArray = [];
 
+// 
+var passwordArray = [];
 
-
-
-
-
-
-var wantSpecial
-var wantCapital
-var wantLower
-var wantNumber 
 
 
 
 function generatePassword() {
   //asking how long does the user wants pass to be
-  var numberofChars = prompt ("How many characters would you like your password to contain?");
+  var userChara = prompt("How many characters would you like your password to contain?");
 
-  if (numberofChars < 8) {
+  if (userChara < 8) {
+    alert("Password length must be atleast 8 characters");
+    return
+  } else if (userChara > 128) {
 
-alert("password length must be at least 8 characters");
-return "Try Again!"
+    alert("Password length must be less than 128 characters");
+    return
+
+
+  } else {
+
+    var ifUpperCase = confirm("Click OK if you would like to include uppercase characters?");
+
+    if (ifUpperCase === true){
+      emptyArray.push(upperCase)
+     }
+     var ifLowerCase = confirm("Click OK if you would like to include lowercase characters?");
+
+     if (ifLowerCase === true){
+      emptyArray.push(lowerCase)
+     }
+
+
+     var ifSpecial = confirm("Click OK if you would like to include special characters?");
+     
+     if (ifSpecial === true){
+      emptyArray.push(ifSpecial)
+     }
+     var ifNumber = confirm("Click OK if you would like to include numerical characters?");
+     
+     if (ifNumber === true){
+      emptyArray.push(ifNumber)
+     }
 
   }
- else if (numberofChars > 128) {
-
-
-  alert("Password length must be less than 129 characters");
-  return "Try Again!"
-
- }
-
- else {
-
-
-confirmSpecialchar = confirm("click ok if you want special characters in your password");
-if (confirmSpecialChar) {
-
-for (var i = 0; i < specialChar.length; i++) {
-
-  combinedArray.push(num[i]);
-}
 
 }
-
- }
-  
-
-
-
-var testAlert = prompt("Enter Number of characters")
-
-
-//logic here
-
-
-wantSpecial = confirm("")
-
-
-
-
-
-
-  return(testAlert)   "This is whats goin to show up on page aka password box"
-}
-
-
-
-
-
-
-
 
 
 
@@ -121,5 +94,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  
+
 
